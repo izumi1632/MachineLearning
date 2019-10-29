84985feb8abe847a4940d250d2ec7a9332450e85
izumi1632 <rendell0119@gmail.com>
2019-10-30 00:17:09 +0900

    numerical_gradient

--- utility/utility.py
+++ utility/utility.py
@@ -76,3 +76,25 @@ def numerical_diff(f, x):
     h = 1e-4 #0.0001
     return (f(x+h) - f(x-h) / (2*h))
 
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+
+def gradient_descent(f, init_x, lr = 0.001, step_num =100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_diff(f,x)
+        x -= lr * grad
+    return x
